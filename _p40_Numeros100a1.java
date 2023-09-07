6f78b44bf6fcc3e49633a243ac6040ae0ddb30f0
Angelseg <huerejoadannn@gmail.com>
2023-09-07 14:01:04 -0600

    Actividad7

---
+++ _p40_Numeros100a1.java
@@ -0,0 +1,12 @@
+//imprimir los numeros de 100 a 1 con ciclo while
+public class _p40_Numeros100a1 {
+    public static void main(String[] args) {
+        int c;
+        c=100;
+        while( c >= 1 ) {
+        System.out.print(c+" ");
+        c--;
+        }
+        System.out.println("\nProceso terminado ...");
+    }
+}
